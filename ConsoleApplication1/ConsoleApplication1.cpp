2d0cefeb105e633f6fd3689c4c7a2065278dba09
dianfitrip <dianfitri683@gmail.com>
2024-03-26 10:31:41 +0700

    membuat variabel, lalu case penjumlahan dan pengurangan

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int bilangan1, bilangan2;
+
+int penjumlahan(int a, int b)
+{
+	return a + b; 
+}
+
+int pengurangan(int a, int b)
+{
+	return a - b;
+}
